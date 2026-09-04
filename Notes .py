19269874002bcbe9b8971f18a2aeb3145ce160a3
amargaribe <garibeamar@gmail.com>
2026-09-05 00:19:01 +0530

    Add files via upload
    My Notes To learn Basic Python...

---
+++ Notes .py
@@ -0,0 +1,36 @@
+#functions > print()
+                  #type()
+                  #lin()
+"""
+operators > means symbol used between two variable and there types of operators is given below.
+1. Arithematic operators ( + ,- ,*, /, **,  %)
+2. Rational operators (== , <, > , <= , != , >= )
+3. Assignment operators (=, +=, -= , *= , **= , /=,%= )
+4. Logical operators (not, and , or)
+"""
+ in case of logical operators = 
+    1. not ( hya madhe not lihun tyasobat jar false type kel tar output tyaulat yet manje true )
+    eg. print(not false) = True
+          print( not True) = False
+          
+    2. and 
+    suppose we have 2 values 
+    value1 = True 
+    value2 = True 
+    print("and operators: ", value1 and value2)
+    Then output will be True because both values are same is called and oprators .
+     insted that if they had diffrent then result will be False. and is called or oprators .
+     
+      1. #Type conversion int into float 
+      2. # type casting
+      eg. a = 10 
+           b = 10.5  
+           print(a + b)  = 20.5 float . 
+   # if there are error then we should do this 
+        a = int("10")        
+        b = 10.5 
+        print(a + b) = 20.5 
+        
+        
+      
+    
