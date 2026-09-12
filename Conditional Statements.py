e1ca31a235b4861d3eda15a5bc2585b0396723a4
amargaribe <garibeamar@gmail.com>
2026-09-12 22:22:40 +0530

    Used If elif else conditions
    Solved practice problems for conditional statements: positive-negative checker, shopping discount, and login system using Pydroid 3.

---
+++ Conditional Statements.py
@@ -0,0 +1,33 @@
+#1
+"""
+ I have one question take any number from user and given condition is if the number is greater than zero then print "number is positive" and if the number is smaller than zero the  print("the number is negetive") otherwise if that number is equal to zero then print("number is zero")
+ """
+ #Considerd number from user is x = 1 let's solve it. 
+ 
+x = 1 
+if x > 0:
+   print("number is positive")
+elif x < 0:
+   print("number is negetive")
+else:
+  print("number is zero")
+ # i got answer(number is positive)
+  
+#2
+
+x = 5000
+if x > 5000:
+   print("you get 20% discount")
+elif x <= 5000:
+   print("you get 10% discount")
+else:
+   print("no discount")
+ #i got answer (you get 10% discount)
+   
+#3
+x = "admin"
+if x == "admin":
+  print("Acess granted welcome admin")
+else:
+  print("access denied wrong username")
+  # i got answer is (acess granted welcome admin)
