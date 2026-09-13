6381823771b3aba14166cdfc789091829ee65183
amargaribe <garibeamar@gmail.com>
2026-09-13 23:59:08 +0530

    Input function
    I am learning python programming language from basics

---
+++ input function.py
@@ -0,0 +1,8 @@
+# practicing input function 
+#1)
+name = input("enter your name;",)
+print("Welcome to python:", name)
+
+#2
+dream = input("enter you dream: ",)
+print("Excillent ! thats nice dream according to future: " , dream)
