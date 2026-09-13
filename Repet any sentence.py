53437160c38af7ad2e2c85ea3d718541f2422b1f
amargaribe <garibeamar@gmail.com>
2026-09-14 00:07:01 +0530

    Update Repet any sentence.py

--- Repet any sentence.py
+++ Repet any sentence.py
@@ -4,4 +4,5 @@ while i  < 10:
     print("Thank You Freind")
 
 
-Then output will be : Thank you freind at infinity 
+Then output will be : "Thank you freind" 
+this sentence will type in a row automatically without stoping till infinity.
